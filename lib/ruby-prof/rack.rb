3dd842a5cc6cd58668c0c2308bb7a8942145a970
Charlie Savage <cfis@zerista.com>
2010-11-06 12:09:09 -0600

    Fix various issues with rack adapter.

--- lib/ruby-prof/rack.rb
+++ lib/ruby-prof/rack.rb
@@ -1,3 +1,5 @@
+require 'tmpdir'
+
 module Rack
   class RubyProf
     def initialize(app)
@@ -13,13 +15,13 @@ module Rack
       result
     end
 
-    def print
-      printers = {::RubyProf::FlatPrinter => 'c:/temp/profile.txt',
-                  ::RubyProf::GraphHtmlPrinter => 'c:/temp/profile.html'}
+    def print(data)
+      printers = {::RubyProf::FlatPrinter => ::File.join(Dir.tmpdir, 'profile.txt'),
+                  ::RubyProf::GraphHtmlPrinter => ::File.join(Dir.tmpdir, 'profile.html')}
 
       printers.each do |printer_klass, file_name|
-        printer = printer_klass.new(result)
-          ::File.open(file_name, 'wb') do |file|
+        printer = printer_klass.new(data)
+        ::File.open(file_name, 'wb') do |file|
           printer.print(file, :min_percent => 0.00000001 )
         end
       end

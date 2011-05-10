9404bbe8aab9eab57181acb3f8e8960f93f0e3da
rogerdpack <rogerpack2005@gmail.com>
2011-05-09 21:00:51 -0600

    version bump

--- ext/ruby_prof/version.h
+++ ext/ruby_prof/version.h
@@ -1,4 +1,4 @@
-#define RUBY_PROF_VERSION  "0.10.6" // for easy parsing from rake files
+#define RUBY_PROF_VERSION  "0.10.7" // for easy parsing from rake files
 #define RUBY_PROF_VERSION_MAJ   0
 #define RUBY_PROF_VERSION_MIN   10
-#define RUBY_PROF_VERSION_MIC   6
+#define RUBY_PROF_VERSION_MIC   7

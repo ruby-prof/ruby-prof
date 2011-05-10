18f7bbc30e521db2243a0fcbf395d6a02072cd1e
rogerdpack <rogerpack2005@gmail.com>
2011-05-09 20:49:27 -0600

    fix REE GC stats for the uppermost method, I think at least

--- test/measurement_test.rb
+++ test/measurement_test.rb
@@ -4,10 +4,12 @@ require 'ruby-prof'
 
 class MeasurementTest < Test::Unit::TestCase
   def setup
+    # also done in C these days...
     GC.enable_stats if GC.respond_to?(:enable_stats)
   end
 
   def teardown
+    # also done in C for normal runs...
     GC.disable_stats if GC.respond_to?(:disable_stats)
   end
 
@@ -72,9 +74,8 @@ class MeasurementTest < Test::Unit::TestCase
   def memory_test_helper
       result = RubyProf.profile {Array.new}
       total = result.threads.values.first.inject(0) { |sum, m| sum + m.total_time }
-
-      assert(total > 0, 'Should measure more than zero kilobytes of memory usage')
-      assert_not_equal(0, total % 1, 'Should not truncate fractional kilobyte measurements')
+      assert(total < 1_000_000, 'Total should not have subtract overflow error')
+      total
   end
 
   if RubyProf::MEMORY
@@ -90,23 +91,10 @@ class MeasurementTest < Test::Unit::TestCase
       u = RubyProf.measure_memory
       assert(u >= t, [t, u].inspect)
       RubyProf::measure_mode = RubyProf::MEMORY
-      memory_test_helper
-    end
-  end
-  
-  def memory_test_helper
-      t = RubyProf.measure_memory
-      assert_kind_of Integer, t
-
-      u = RubyProf.measure_memory
-      assert(u >= t, [t, u].inspect)
-
-      RubyProf::measure_mode = RubyProf::MEMORY
-      result = RubyProf.profile {Array.new}
-      total = result.threads.values.first.inject(0) { |sum, m| sum + m.total_time }
-
+      total = memory_test_helper
       assert(total > 0, 'Should measure more than zero kilobytes of memory usage')
-      assert_not_equal(0, total % 1, 'Should not truncate fractional kilobyte measurements ' + total.inspect)
+      assert_not_equal(0, total % 1, 'Should not truncate fractional kilobyte measurements')
+    end
   end
 
   if RubyProf::GC_RUNS

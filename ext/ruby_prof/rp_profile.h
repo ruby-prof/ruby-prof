c741fe5760479081841e51aa3b9f1571c336daa6
Charlie Savage <cfis@zerista.com>
2011-11-06 01:39:39 -0700

    New profile and measure files.

---
+++ ext/ruby_prof/rp_profile.h
@@ -0,0 +1,18 @@
+/* Copyright (C) 2005-2011 Shugo Maeda <shugo@ruby-lang.org> and Charlie Savage <cfis@savagexi.com>
+   Please see the LICENSE file for copyright and distribution information */
+
+#ifndef __RP_PROFILE__
+#define __RP_PROFILE__
+
+#include "ruby_prof.h"
+
+extern VALUE cResult;
+
+typedef struct {
+    VALUE threads;
+} prof_result_t;
+
+void rp_init_result();
+VALUE prof_result_new(st_table* threads_tbl);
+
+#endif

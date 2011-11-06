eb77660094082fd7c18801b4e50d74d813639527
Charlie Savage <cfis@zerista.com>
2011-11-06 01:41:26 -0700

    Rename test to match file name.

--- test/exec_test.rb
+++ test/exec_test.rb
@@ -5,7 +5,7 @@ require './test_helper'
 
 # --  Test for bug when it loads with no frames
 
-class EnumerableTest < Test::Unit::TestCase
+class ExecTest < Test::Unit::TestCase
   def test_being_able_to_run_its_binary
     Dir.chdir(File.dirname(__FILE__)) do
       assert system(OS.ruby_bin + " ruby-prof-bin do_nothing.rb")

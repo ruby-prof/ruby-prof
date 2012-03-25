1e9619ab76e852a098718dc1b4353199734f3d5f
Charlie Savage <cfis@zerista.com>
2012-03-24 23:00:45 -0600

    Update rack adapter to be more flexible.

--- lib/ruby-prof/rack.rb
+++ lib/ruby-prof/rack.rb
@@ -1,13 +1,17 @@
 # encoding: utf-8
+require 'tmpdir'
 
 module Rack
   class RubyProf
-    def initialize(app)
+    def initialize(app, options = {})
       @app = app
+      @options = options
+      @options[:min_percent] ||= 0.01
+      @tmpdir = options[:path] || Dir.tmpdir
     end
 
     def call(env)
-      ::RubyProf.start
+      ::RubyProf::Profile.start
       result = @app.call(env)
       data = ::RubyProf.stop
 
@@ -16,14 +20,13 @@ module Rack
     end
 
     def print(data)
-      require 'tmpdir' # late require so we load on demand only
-      printers = {::RubyProf::FlatPrinter => ::File.join(Dir.tmpdir, 'profile.txt'),
-                  ::RubyProf::GraphHtmlPrinter => ::File.join(Dir.tmpdir, 'profile.html')}
+      printers = {::RubyProf::FlatPrinter => ::File.join(@tmpdir, 'profile.txt'),
+                  ::RubyProf::GraphHtmlPrinter => ::File.join(@tmpdir, 'profile.html')}
 
       printers.each do |printer_klass, file_name|
         printer = printer_klass.new(data)
         ::File.open(file_name, 'wb') do |file|
-          printer.print(file, :min_percent => 0.00000001 )
+          printer.print(file, @options)
         end
       end
     end

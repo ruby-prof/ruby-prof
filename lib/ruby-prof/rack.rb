90429ff5e23f54e2a951b336ab9cfa7cba19a226
Charlie Savage <cfis@zerista.com>
2012-03-26 11:32:46 -0600

    Update rack adapter to make printers configurable and add ability to skip paths, like assets.

--- lib/ruby-prof/rack.rb
+++ lib/ruby-prof/rack.rb
@@ -8,24 +8,36 @@ module Rack
       @options = options
       @options[:min_percent] ||= 1
       @tmpdir = options[:path] || Dir.tmpdir
-      @printer_klasses = {::RubyProf::FlatPrinter => 'flat.txt',
-                          ::RubyProf::GraphPrinter => 'graph.txt',
-                          ::RubyProf::GraphHtmlPrinter => 'graph.html',
-                          ::RubyProf::CallStackPrinter => 'call_stack.html'}
+      @printer_klasses = @options[:printers]  || {::RubyProf::FlatPrinter => 'flat.txt',
+                                                  ::RubyProf::GraphPrinter => 'graph.txt',
+                                                  ::RubyProf::GraphHtmlPrinter => 'graph.html',
+                                                  ::RubyProf::CallStackPrinter => 'call_stack.html'}
+
+      @printer_klasses = @options[:printers]  || {::RubyProf::FlatPrinter => 'flat.txt',
+                                                  ::RubyProf::GraphPrinter => 'graph.txt',
+                                                  ::RubyProf::GraphHtmlPrinter => 'graph.html',
+                                                  ::RubyProf::CallStackPrinter => 'call_stack.html'}
+
+      @skip_paths = options[:skip_paths] || [%r{^/assets}, %r{\.css$}, %r{\.js}, %r{\.png}, %r{\.jpeg}]
     end
 
     def call(env)
-      result = nil
-      data = ::RubyProf::Profile.profile do
-        result = @app.call(env)
-      end
-
       request = Rack::Request.new(env)
-      path = request.path.gsub('/', '-')
-      path.slice!(0)
 
-      print(data, path)
-      result
+      if @skip_paths.any? {|skip_path| skip_path =~ request.path}
+        @app.call(env)
+      else
+        result = nil
+        data = ::RubyProf::Profile.profile do
+          result = @app.call(env)
+        end
+
+        path = request.path.gsub('/', '-')
+        path.slice!(0)
+
+        print(data, path)
+        result
+      end
     end
 
     def print(data, path)

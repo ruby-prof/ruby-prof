c7d2f7538bab6f891bb6adc88645c1d2cc79e65e
Charlie Savage <cfis@zerista.com>
2012-03-25 22:45:08 -0600

    More call info cleanup.

--- ext/ruby_prof/rp_call_info.c
+++ ext/ruby_prof/rp_call_info.c
@@ -7,11 +7,101 @@
 
 VALUE cCallInfo;
 
-/* =======  Call Info   ========*/
+
+// Forward declarations
+st_table * call_info_table_create();
+void call_info_table_free(st_table *table);
+
+
+/* =======  prof_call_info_t   ========*/
+prof_call_info_t *
+prof_call_info_create(prof_method_t* method, prof_call_info_t* parent)
+{
+    prof_call_info_t *result = ALLOC(prof_call_info_t);
+    result->object = Qnil;
+    result->target = method;
+    result->parent = parent;
+    result->call_infos = call_info_table_create();
+    result->children = Qnil;
+
+    result->called = 0;
+    result->total_time = 0;
+    result->self_time = 0;
+    result->wait_time = 0;
+    result->line = 0;
+    return result;
+}
+
+static void
+prof_call_info_free(prof_call_info_t *call_info)
+{
+	/* Has this thread object been accessed by Ruby?  If
+	   yes clean it up so to avoid a segmentation fault. */
+	if (call_info->object != Qnil)
+	{
+		RDATA(call_info->object)->data = NULL;
+		RDATA(call_info->object)->dfree = NULL;
+		RDATA(call_info->object)->dmark = NULL;
+    }
+	call_info->object = Qnil;
+
+	call_info_table_free(call_info->call_infos);
+	xfree(call_info);
+}
+
+static void
+prof_call_info_mark(prof_call_info_t *call_info)
+{
+	if (call_info->object)
+		rb_gc_mark(call_info->children);
+
+	if (call_info->children)
+		rb_gc_mark(call_info->children);
+}
+
+
+static VALUE
+prof_call_info_wrap(prof_call_info_t *call_info)
+{
+  if (call_info->object == Qnil)
+  {
+    call_info->object = Data_Wrap_Struct(cCallInfo, prof_call_info_mark, prof_call_info_free, call_info);
+  }
+  return call_info->object;
+}
+
+static prof_call_info_t *
+prof_get_call_info(VALUE self)
+{
+    /* Can't use Data_Get_Struct because that triggers the event hook
+       ending up in endless recursion. */
+	prof_call_info_t* result = DATA_PTR(self);
+
+	if (!result)
+	    rb_raise(rb_eRuntimeError, "This RubyProf::CallInfo instance has already been freed, likely because its profile has been freed.");
+
+   return result;
+}
+
+/* =======  Call Info Table   ========*/
 st_table *
 call_info_table_create()
 {
   return st_init_table(&type_method_hash);
+}
+
+static int
+call_info_table_free_iterator(st_data_t key, st_data_t value, st_data_t dummy)
+{
+    prof_call_info_free((prof_call_info_t*)value);
+    return ST_CONTINUE;
+}
+
+void
+call_info_table_free(st_table *table)
+{
+  //  st_foreach(table, call_info_table_free_iterator, 0);
+    st_free_table(table);
 }
 
 size_t
@@ -34,73 +124,13 @@ call_info_table_lookup(st_table *table, const prof_method_key_t *key)
     }
 }
 
-static void
-call_info_table_free(st_table *table)
-{
-    st_free_table(table);
-}
+
+/* =======  RubyProf::CallInfo   ========*/
 
 /* Document-class: RubyProf::CallInfo
 RubyProf::CallInfo is a helper class used by RubyProf::MethodInfo
 to keep track of which child methods were called and how long
 they took to execute. */
-
-/* :nodoc: */
-prof_call_info_t *
-prof_call_info_create(prof_method_t* method, prof_call_info_t* parent)
-{
-    prof_call_info_t *result = ALLOC(prof_call_info_t);
-    result->object = Qnil;
-    result->target = method;
-    result->parent = parent;
-    result->call_infos = call_info_table_create();
-    result->children = Qnil;
-
-    result->called = 0;
-    result->total_time = 0;
-    result->self_time = 0;
-    result->wait_time = 0;
-    result->line = 0;
-    return result;
-}
-
-static void
-prof_call_info_mark(prof_call_info_t *call_info)
-{
-	if (call_info->object)
-		rb_gc_mark(call_info->children);
-
-	if (call_info->children)
-		rb_gc_mark(call_info->children);
-}
-
-static void
-prof_call_info_free(prof_call_info_t *call_info)
-{
-  call_info_table_free(call_info->call_infos);
-  xfree(call_info);
-}
-
-static VALUE
-prof_call_info_wrap(prof_call_info_t *call_info)
-{
-  if (call_info->object == Qnil)
-  {
-    call_info->object = Data_Wrap_Struct(cCallInfo, prof_call_info_mark, prof_call_info_free, call_info);
-  }
-  return call_info->object;
-}
-
-static prof_call_info_t *
-prof_get_call_info_result(VALUE obj)
-{
-    if (BUILTIN_TYPE(obj) != T_DATA)
-    {
-        /* Should never happen */
-      rb_raise(rb_eTypeError, "Not a call info object");
-    }
-    return (prof_call_info_t *) DATA_PTR(obj);
-}
 
 
 /* call-seq:
@@ -114,7 +144,7 @@ prof_call_info_target(VALUE self)
        about the GC.  We will wrap the method_info but provide no
        free method so the underlying object is not freed twice! */
 
-    prof_call_info_t *result = prof_get_call_info_result(self);
+    prof_call_info_t *result = prof_get_call_info(self);
     return prof_method_wrap(result->target);
 }
 
@@ -125,7 +155,7 @@ Returns the total amount of times this method was called. */
 static VALUE
 prof_call_info_called(VALUE self)
 {
-    prof_call_info_t *result = prof_get_call_info_result(self);
+    prof_call_info_t *result = prof_get_call_info(self);
     return INT2NUM(result->called);
 }
 
@@ -136,7 +166,7 @@ Sets the call count to n. */
 static VALUE
 prof_call_info_set_called(VALUE self, VALUE called)
 {
-    prof_call_info_t *result = prof_get_call_info_result(self);
+    prof_call_info_t *result = prof_get_call_info(self);
     result->called = NUM2INT(called);
     return called;
 }
@@ -148,7 +178,7 @@ prof_call_info_set_called(VALUE self, VALUE called)
 static VALUE
 prof_call_info_depth(VALUE self)
 {
-  prof_call_info_t *result = prof_get_call_info_result(self);
+  prof_call_info_t *result = prof_get_call_info(self);
   return rb_int_new(result->depth);
 }
 
@@ -159,7 +189,7 @@ prof_call_info_depth(VALUE self)
 static VALUE
 prof_call_info_line(VALUE self)
 {
-  prof_call_info_t *result = prof_get_call_info_result(self);
+  prof_call_info_t *result = prof_get_call_info(self);
   return rb_int_new(result->line);
 }
 
@@ -170,7 +200,7 @@ Returns the total amount of time spent in this method and its children. */
 static VALUE
 prof_call_info_total_time(VALUE self)
 {
-    prof_call_info_t *result = prof_get_call_info_result(self);
+    prof_call_info_t *result = prof_get_call_info(self);
     return rb_float_new(result->total_time);
 }
 
@@ -181,8 +211,8 @@ adds total time time from call_info to self. */
 static VALUE
 prof_call_info_add_total_time(VALUE self, VALUE other)
 {
-    prof_call_info_t *result = prof_get_call_info_result(self);
-    prof_call_info_t *other_info = prof_get_call_info_result(other);
+    prof_call_info_t *result = prof_get_call_info(self);
+    prof_call_info_t *other_info = prof_get_call_info(other);
 
     result->total_time += other_info->total_time;
     return Qnil;
@@ -195,7 +225,7 @@ Returns the total amount of time spent in this method. */
 static VALUE
 prof_call_info_self_time(VALUE self)
 {
-    prof_call_info_t *result = prof_get_call_info_result(self);
+    prof_call_info_t *result = prof_get_call_info(self);
 
     return rb_float_new(result->self_time);
 }
@@ -207,8 +237,8 @@ adds self time from call_info to self. */
 static VALUE
 prof_call_info_add_self_time(VALUE self, VALUE other)
 {
-    prof_call_info_t *result = prof_get_call_info_result(self);
-    prof_call_info_t *other_info = prof_get_call_info_result(other);
+    prof_call_info_t *result = prof_get_call_info(self);
+    prof_call_info_t *other_info = prof_get_call_info(other);
 
     result->self_time += other_info->self_time;
     return Qnil;
@@ -221,7 +251,7 @@ Returns the total amount of time this method waited for other threads. */
 static VALUE
 prof_call_info_wait_time(VALUE self)
 {
-    prof_call_info_t *result = prof_get_call_info_result(self);
+    prof_call_info_t *result = prof_get_call_info(self);
 
     return rb_float_new(result->wait_time);
 }
@@ -234,8 +264,8 @@ adds wait time from call_info to self. */
 static VALUE
 prof_call_info_add_wait_time(VALUE self, VALUE other)
 {
-    prof_call_info_t *result = prof_get_call_info_result(self);
-    prof_call_info_t *other_info = prof_get_call_info_result(other);
+    prof_call_info_t *result = prof_get_call_info(self);
+    prof_call_info_t *other_info = prof_get_call_info(other);
 
     result->wait_time += other_info->wait_time;
     return Qnil;
@@ -248,7 +278,7 @@ Returns the call_infos parent call_info object (the method that called this meth
 static VALUE
 prof_call_info_parent(VALUE self)
 {
-    prof_call_info_t *result = prof_get_call_info_result(self);
+    prof_call_info_t *result = prof_get_call_info(self);
     if (result->parent)
       return prof_call_info_wrap(result->parent);
     else
@@ -262,11 +292,11 @@ Changes the parent of self to new_parent and returns it.*/
 static VALUE
 prof_call_info_set_parent(VALUE self, VALUE new_parent)
 {
-    prof_call_info_t *result = prof_get_call_info_result(self);
+    prof_call_info_t *result = prof_get_call_info(self);
     if (new_parent == Qnil)
       result->parent = NULL;
     else
-      result->parent = prof_get_call_info_result(new_parent);
+      result->parent = prof_get_call_info(new_parent);
     return prof_call_info_parent(self);
 }
 
@@ -287,7 +317,7 @@ called (ie, children).*/
 static VALUE
 prof_call_info_children(VALUE self)
 {
-    prof_call_info_t *call_info = prof_get_call_info_result(self);
+    prof_call_info_t *call_info = prof_get_call_info(self);
     if (call_info->children == Qnil)
     {
       call_info->children = rb_ary_new();

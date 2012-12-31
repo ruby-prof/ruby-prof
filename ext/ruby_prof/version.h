97529d38d1c8603142ae9e071d4a14dcd11b2b47
Charlie Savage <cfis@zerista.com>
2012-12-31 13:42:24 -0800

    Update version and release notes.

--- ext/ruby_prof/version.h
+++ ext/ruby_prof/version.h
@@ -1,7 +1,7 @@
 /* Copyright (C) 2005-2011 Shugo Maeda <shugo@ruby-lang.org> and Charlie Savage <cfis@savagexi.com>
    Please see the LICENSE file for copyright and distribution information */
 
-#define RUBY_PROF_VERSION  "0.11.3" // as a string, for easy parsing from rake files
+#define RUBY_PROF_VERSION  "0.11.4" // as a string, for easy parsing from rake files
 #define RUBY_PROF_VERSION_MAJ   0
 #define RUBY_PROF_VERSION_MIN   11
-#define RUBY_PROF_VERSION_MIC   2
+#define RUBY_PROF_VERSION_MIC   4

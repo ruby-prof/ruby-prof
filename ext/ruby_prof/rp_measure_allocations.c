f03bb72aabff4eb624c003356d7d6dbaa906092f
Charlie Savage <cfis@zerista.com>
2013-01-07 00:20:42 -0800

    Update copyright to 2013.

--- ext/ruby_prof/rp_measure_allocations.c
+++ ext/ruby_prof/rp_measure_allocations.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2005-2011 Shugo Maeda <shugo@ruby-lang.org> and Charlie Savage <cfis@savagexi.com>
+/* Copyright (C) 2005-2013 Shugo Maeda <shugo@ruby-lang.org> and Charlie Savage <cfis@savagexi.com>
    Please see the LICENSE file for copyright and distribution information */
 
 /* :nodoc: */
--- ext/ruby_prof/version.h
+++ ext/ruby_prof/version.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2005-2011 Shugo Maeda <shugo@ruby-lang.org> and Charlie Savage <cfis@savagexi.com>
+/* Copyright (C) 2005-2013 Shugo Maeda <shugo@ruby-lang.org> and Charlie Savage <cfis@savagexi.com>
    Please see the LICENSE file for copyright and distribution information */
 
 #define RUBY_PROF_VERSION  "0.12.0" // as a string, for easy parsing from rake files

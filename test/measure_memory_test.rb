76b8019c216959f755c8548addbc918591cc41ef
Stefan Kaes <stefan.kaes@xing.com>
2013-02-03 11:48:32 +0100

    fixed memory measurement test

--- test/measure_memory_test.rb
+++ test/measure_memory_test.rb
@@ -4,12 +4,7 @@
 require File.expand_path('../test_helper', __FILE__)
 
 class MeasureMemoryTest < Test::Unit::TestCase
-  def memory_test_helper
-    result = RubyProf.profile {Array.new}
-    total = result.threads.values.first.inject(0) { |sum, m| sum + m.total_time }
-    assert(total < 1_000_000, 'Total should not have subtract overflow error')
-    total
-  end
+  include MemoryTestHelper
 
   def test_memory_mode
     RubyProf::measure_mode = RubyProf::MEMORY
@@ -23,10 +18,10 @@ class MeasureMemoryTest < Test::Unit::TestCase
   if RubyProf::MEMORY_ENABLED
     def test_memory
       t = RubyProf.measure_memory
-      assert_kind_of Integer, t
+      assert_kind_of Float, t
 
       u = RubyProf.measure_memory
-      assert(u >= t, [t, u].inspect)
+      assert(u > t, [t, u].inspect)
       RubyProf::measure_mode = RubyProf::MEMORY
       total = memory_test_helper
       assert(total > 0, 'Should measure more than zero kilobytes of memory usage')

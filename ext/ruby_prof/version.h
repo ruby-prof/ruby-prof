befe13fdb4a89a3b86d15552c9bb732a9f9c3875
Stefan Kaes <stefan.kaes@xing.com>
2013-12-14 08:39:26 +0100

    bumped version to 0.13.1

--- ext/ruby_prof/version.h
+++ ext/ruby_prof/version.h
@@ -1,7 +1,7 @@
 /* Copyright (C) 2005-2013 Shugo Maeda <shugo@ruby-lang.org> and Charlie Savage <cfis@savagexi.com>
    Please see the LICENSE file for copyright and distribution information */
 
-#define RUBY_PROF_VERSION  "0.13.0" // as a string, for easy parsing from rake files
+#define RUBY_PROF_VERSION  "0.13.1" // as a string, for easy parsing from rake files
 #define RUBY_PROF_VERSION_MAJ   0
 #define RUBY_PROF_VERSION_MIN   13
-#define RUBY_PROF_VERSION_MIC   0
+#define RUBY_PROF_VERSION_MIC   1

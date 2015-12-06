2d07e8e0f8842f834839fae0dd702ad2d16539ea
Stefan Kaes <stefan.kaes@xing.com>
2015-12-06 14:09:20 +0100

    updated flat example. closes #169

--- test/printers_test.rb
+++ test/printers_test.rb
@@ -11,7 +11,7 @@ class PrintersTest < TestCase
     # WALL_TIME so we can use sleep in our test and get same measurements on linux and windows
     RubyProf::measure_mode = RubyProf::WALL_TIME
     @result = RubyProf.profile do
-      run_primes(200)
+      run_primes(1000, 5000)
     end
   end
 

3adb1dc21da5af94840dd2c65cdb903a77b0d77d
Stefan Kaes <stefan.kaes@xing.com>
2016-01-02 16:43:50 +0100

    added method to calculate total wait time of a thread

--- lib/ruby-prof/thread.rb
+++ lib/ruby-prof/thread.rb
@@ -26,5 +26,16 @@ module RubyProf
         sum
       end
     end
+
+    def wait_time
+      # wait_time, like self:time, is always method local
+      # thus we need to sum over all methods and call infos
+      self.methods.inject(0) do |sum, method_info|
+        method_info.call_infos.each do |call_info|
+          sum += call_info.wait_time
+        end
+        sum
+      end
+    end
   end
 end

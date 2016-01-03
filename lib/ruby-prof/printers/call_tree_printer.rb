85fe2c40f998ae29cec62c283af9c58d304fd5b4
Stefan Kaes <stefan.kaes@xing.com>
2016-01-03 14:43:00 +0100

    support separating threads in callgrind dumps
    according to http://valgrind.org/docs/manual/cl-format.html
    a callgrind file can have multiple parts, one for each thread.
    
    if this doesn't work we will need to dump one file for each thread.

--- lib/ruby-prof/printers/call_tree_printer.rb
+++ lib/ruby-prof/printers/call_tree_printer.rb
@@ -20,40 +20,42 @@ module RubyProf
     def print(output = STDOUT, options = {})
       @output = output
       setup_options(options)
-
-      # add a header - this information is somewhat arbitrary
-      @output << "events: "
-      case RubyProf.measure_mode
-        when RubyProf::PROCESS_TIME
-          @value_scale = RubyProf::CLOCKS_PER_SEC;
-          @output << 'process_time'
-        when RubyProf::WALL_TIME
-          @value_scale = 1_000_000
-          @output << 'wall_time'
-        when RubyProf.const_defined?(:CPU_TIME) && RubyProf::CPU_TIME
-          @value_scale = RubyProf.cpu_frequency
-          @output << 'cpu_time'
-        when RubyProf.const_defined?(:ALLOCATIONS) && RubyProf::ALLOCATIONS
-          @value_scale = 1
-          @output << 'allocations'
-        when RubyProf.const_defined?(:MEMORY) && RubyProf::MEMORY
-          @value_scale = 1
-          @output << 'memory'
-        when RubyProf.const_defined?(:GC_RUNS) && RubyProf::GC_RUNS
-          @value_scale = 1
-          @output << 'gc_runs'
-        when RubyProf.const_defined?(:GC_TIME) && RubyProf::GC_TIME
-          @value_scale = 1000000
-          @output << 'gc_time'
-        else
-          raise "Unknown measure mode: #{RubyProf.measure_mode}"
-      end
-      @output << "\n\n"
-
+      determine_event_specification_and_value_scale
       print_threads
     end
 
+    def determine_event_specification_and_value_scale
+      @event_specification = "events: "
+      case RubyProf.measure_mode
+        when RubyProf::PROCESS_TIME
+          @value_scale = RubyProf::CLOCKS_PER_SEC
+          @event_specification << 'process_time'
+        when RubyProf::WALL_TIME
+          @value_scale = 1_000_000
+          @event_specification << 'wall_time'
+        when RubyProf.const_defined?(:CPU_TIME) && RubyProf::CPU_TIME
+          @value_scale = RubyProf.cpu_frequency
+          @event_specification << 'cpu_time'
+        when RubyProf.const_defined?(:ALLOCATIONS) && RubyProf::ALLOCATIONS
+          @value_scale = 1
+          @event_specification << 'allocations'
+        when RubyProf.const_defined?(:MEMORY) && RubyProf::MEMORY
+          @value_scale = 1
+          @event_specification << 'memory'
+        when RubyProf.const_defined?(:GC_RUNS) && RubyProf::GC_RUNS
+          @value_scale = 1
+          @event_specification << 'gc_runs'
+        when RubyProf.const_defined?(:GC_TIME) && RubyProf::GC_TIME
+          @value_scale = 1000000
+          @event_specification << 'gc_time'
+        else
+          raise "Unknown measure mode: #{RubyProf.measure_mode}"
+      end
+    end
+
     def print_threads
+      # TODO: merge fibers of a given thread
+      # kcachegrind doesn't know about fibers
       @result.threads.each do |thread|
         print_thread(thread)
       end
@@ -68,25 +70,35 @@ module RubyProf
     end
 
     def print_thread(thread)
+      print_headers(thread)
       thread.methods.reverse_each do |method|
-        # Print out the file and method name
-        @output << "fl=#{file(method)}\n"
-        @output << "fn=#{method_name(method)}\n"
-
-        # Now print out the function line number and its self time
-        @output << "#{method.line} #{convert(method.self_time)}\n"
-
-        # Now print out all the children methods
-        method.children.each do |callee|
-          @output << "cfl=#{file(callee.target)}\n"
-          @output << "cfn=#{method_name(callee.target)}\n"
-          @output << "calls=#{callee.called} #{callee.line}\n"
-
-          # Print out total times here!
-          @output << "#{callee.line} #{convert(callee.total_time)}\n"
-        end
-      @output << "\n"
+        print_method(method)
       end
-    end #end print_methods
-  end # end class
-end # end packages
+    end
+
+    def print_headers(thread)
+      @output << "#{@event_specification}\n"
+      @output << "thread: #{thread.id}\n\n"
+    end
+
+    def print_method(method)
+      # Print out the file and method name
+      @output << "fl=#{file(method)}\n"
+      @output << "fn=#{method_name(method)}\n"
+
+      # Now print out the function line number and its self time
+      @output << "#{method.line} #{convert(method.self_time)}\n"
+
+      # Now print out all the children methods
+      method.children.each do |callee|
+        @output << "cfl=#{file(callee.target)}\n"
+        @output << "cfn=#{method_name(callee.target)}\n"
+        @output << "calls=#{callee.called} #{callee.line}\n"
+
+        # Print out total times here!
+        @output << "#{callee.line} #{convert(callee.total_time)}\n"
+      end
+      @output << "\n"
+    end
+  end
+end

6f74c6ce6ac093ba1887f06a32a5b17b2df4c07f
Stefan Kaes <stefan.kaes@xing.com>
2017-12-16 18:04:48 +0100

    adapt line number test to ruby 2.5

--- test/line_number_test.rb
+++ test/line_number_test.rb
@@ -34,7 +34,11 @@ class LineNumbersTest < TestCase
 
     method = methods[1]
     assert_equal('LineNumbers#method2', method.full_name)
-    assert_equal(10, method.line)
+    if RUBY_VERSION < "2.5"
+      assert_equal(10, method.line)
+    else
+      assert_equal(11, method.line)
+    end
 
     method = methods[2]
     assert_equal('LineNumbers#method1', method.full_name)
@@ -62,10 +66,15 @@ class LineNumbersTest < TestCase
 
     method = methods[1]
     assert_equal('LineNumbers#method3', method.full_name)
-    assert_equal(14, method.line)
+    if RUBY_VERSION < "2.5"
+      assert_equal(14, method.line)
+    else
+      assert_equal(15, method.line)
+    end
 
     method = methods[2]
     assert_equal('LineNumbersTest#test_c_function', method.full_name)
-    assert_equal(48, method.line)
+    assert_equal(52, method.line)
   end
+
 end

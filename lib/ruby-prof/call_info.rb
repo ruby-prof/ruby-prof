9847ad5af423f09c17c94ddb052eb9fbd515b3e2
Charlie Savage <cfis@zerista.com>
2019-06-07 15:07:34 -0600

    Remove old method elimination code, add <=> operator for sorting.

--- lib/ruby-prof/call_info.rb
+++ lib/ruby-prof/call_info.rb
@@ -20,24 +20,14 @@ module RubyProf
       super + "(#{target.full_name}, d: #{depth}, c: #{called}, tt: #{total_time}, st: #{self_time}, ct: #{children_time})"
     end
 
-    # eliminate call info from the call tree.
-    # adds self and wait time to parent and attaches called methods to parent.
-    # merges call trees for methods called from both praent end self.
-    def eliminate!
-      # puts "eliminating #{self}"
-      return unless parent
-      parent.add_self_time(self)
-      parent.add_wait_time(self)
-      children.each do |kid|
-        if call = parent.find_call(kid)
-          call.merge_call_tree(kid)
-        else
-          parent.children << kid
-          # $stderr.puts "setting parent of #{kid}\nto #{parent}"
-          kid.parent = parent
-        end
+    def <=>(other)
+      if self.total_time < other.total_time
+        -1
+      elsif self.total_time > other.total_time
+        1
+      else
+        self.target.full_name <=> other.target.full_name
       end
-      parent.children.delete(self)
     end
 
     # find a specific call in list of children. returns nil if not found.
@@ -47,26 +37,6 @@ module RubyProf
       matching = children.select { |kid| kid.target == other.target }
       raise "inconsistent call tree" unless matching.size <= 1
       matching.first
-    end
-
-    # merge two call trees. adds self, wait, and total time of other to self and merges children of other into children of self.
-    def merge_call_tree(other)
-      # $stderr.puts "merging #{self}\nand #{other}"
-      self.called += other.called
-      add_self_time(other)
-      add_wait_time(other)
-      add_total_time(other)
-      other.children.each do |other_kid|
-        if kid = find_call(other_kid)
-          # $stderr.puts "merging kids"
-          kid.merge_call_tree(other_kid)
-        else
-          other_kid.parent = self
-          children << other_kid
-        end
-      end
-      other.children.clear
-      other.target.call_infos.delete(other)
     end
   end
 end

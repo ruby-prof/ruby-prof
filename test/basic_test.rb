794311656bcb4cd5e1de0bb08d46ed85629139cc
Charlie Savage <cfis@zerista.com>
2019-06-12 11:14:11 -0700

    Remove debug code.

--- test/basic_test.rb
+++ test/basic_test.rb
@@ -14,127 +14,115 @@ class BasicTest < TestCase
     RubyProf::C1.sleep_wait
   end
 
-  def test_sleep
-    profile = RubyProf.profile do
-      sleep(2)
-    end
-    
-    printer = RubyProf::GraphHtmlPrinter.new(profile)
-    File.open('c:/temp/graph.html', 'wb') do |file|
-      printer.print(file)
-    end
-
+  def test_running
+    assert(!RubyProf.running?)
+    RubyProf.start
+    assert(RubyProf.running?)
+    RubyProf.stop
+    assert(!RubyProf.running?)
   end
 
-  # def test_running
-  #   assert(!RubyProf.running?)
-  #   RubyProf.start
-  #   assert(RubyProf.running?)
-  #   RubyProf.stop
-  #   assert(!RubyProf.running?)
-  # end
-  #
-  # def test_double_profile
-  #   RubyProf.start
-  #   assert_raises(RuntimeError) do
-  #     RubyProf.start
-  #   end
-  #   RubyProf.stop
-  # end
-  #
-  # def test_no_block
-  #   assert_raises(ArgumentError) do
-  #     RubyProf.profile
-  #   end
-  # end
-  #
-  # def test_traceback
-  #   RubyProf.start
-  #   assert_raises(NoMethodError) do
-  #     RubyProf.xxx
-  #   end
-  #
-  #   RubyProf.stop
-  # end
-  #
-  # def test_leave_method
-  #   start
-  #   sleep 0.15
-  #   profile = RubyProf.stop
-  #
-  #   assert_equal(1, profile.threads.count)
-  #
-  #   thread = profile.threads.first
-  #   assert_in_delta(0.25, thread.total_time, 0.015)
-  #
-  #   root_methods = thread.root_methods.sort
-  #   assert_equal(2, root_methods.count)
-  #   assert_equal("BasicTest#start", root_methods[0].full_name)
-  #   assert_equal("BasicTest#test_leave_method", root_methods[1].full_name)
-  #
-  #   assert_equal(4, thread.methods.length)
-  #   methods = profile.threads.first.methods.sort
-  #
-  #   # Check times
-  #   assert_equal("<Class::RubyProf::C1>#sleep_wait", methods[0].full_name)
-  #   assert_in_delta(0.1, methods[0].total_time, 0.015)
-  #   assert_in_delta(0.0,  methods[0].wait_time, 0.015)
-  #   assert_in_delta(0.0,  methods[0].self_time, 0.015)
-  #
-  #   assert_equal("BasicTest#start", methods[1].full_name)
-  #   assert_in_delta(0.1, methods[1].total_time, 0.015)
-  #   assert_in_delta(0.0, methods[1].wait_time, 0.015)
-  #   assert_in_delta(0.0, methods[1].self_time, 0.015)
-  #
-  #   assert_equal("BasicTest#test_leave_method", methods[2].full_name)
-  #   assert_in_delta(0.15, methods[2].total_time, 0.015)
-  #   assert_in_delta(0.0, methods[2].wait_time, 0.015)
-  #   assert_in_delta(0.0, methods[2].self_time, 0.015)
-  #
-  #   assert_equal("Kernel#sleep", methods[3].full_name)
-  #   assert_in_delta(0.25, methods[3].total_time, 0.015)
-  #   assert_in_delta(0.0, methods[3].wait_time, 0.015)
-  #   assert_in_delta(0.25, methods[3].self_time, 0.015)
-  # end
-  #
-  # def test_leave_method_2
-  #   start
-  #   RubyProf::C1.sleep_wait
-  #   RubyProf::C1.sleep_wait
-  #   profile = RubyProf.stop
-  #
-  #   assert_equal(1, profile.threads.count)
-  #
-  #   thread = profile.threads.first
-  #   assert_in_delta(0.3, thread.total_time, 0.015)
-  #
-  #   root_methods = thread.root_methods.sort
-  #   assert_equal(2, root_methods.count)
-  #   assert_equal("BasicTest#start", root_methods[0].full_name)
-  #   assert_equal("BasicTest#test_leave_method_2", root_methods[1].full_name)
-  #
-  #   assert_equal(4, thread.methods.length)
-  #   methods = profile.threads.first.methods.sort
-  #
-  #   # Check times
-  #   assert_equal("BasicTest#start", methods[0].full_name)
-  #   assert_in_delta(0.1, methods[0].total_time, 0.015)
-  #   assert_in_delta(0.0, methods[0].wait_time, 0.015)
-  #   assert_in_delta(0.0, methods[0].self_time, 0.015)
-  #
-  #   assert_equal("BasicTest#test_leave_method_2", methods[1].full_name)
-  #   assert_in_delta(0.2, methods[1].total_time, 0.015)
-  #   assert_in_delta(0.0, methods[1].wait_time, 0.015)
-  #   assert_in_delta(0.0, methods[1].self_time, 0.015)
-  #
-  #   assert_equal("Kernel#sleep", methods[2].full_name)
-  #   assert_in_delta(0.3, methods[2].total_time, 0.015)
-  #   assert_in_delta(0.0, methods[2].wait_time, 0.015)
-  #   assert_in_delta(0.3, methods[2].self_time, 0.015)
-  #
-  #   assert_equal("<Class::RubyProf::C1>#sleep_wait", methods[3].full_name)
-  #   assert_in_delta(0.3, methods[3].total_time, 0.015)
-  #   assert_in_delta(0.0, methods[3].wait_time, 0.015)
-  #   assert_in_delta(0.0, methods[3].self_time, 0.015)
-  # end
+  def test_double_profile
+    RubyProf.start
+    assert_raises(RuntimeError) do
+      RubyProf.start
+    end
+    RubyProf.stop
+  end
+
+  def test_no_block
+    assert_raises(ArgumentError) do
+      RubyProf.profile
+    end
+  end
+
+  def test_traceback
+    RubyProf.start
+    assert_raises(NoMethodError) do
+      RubyProf.xxx
+    end
+
+    RubyProf.stop
+  end
+
+  def test_leave_method
+    start
+    sleep 0.15
+    profile = RubyProf.stop
+
+    assert_equal(1, profile.threads.count)
+
+    thread = profile.threads.first
+    assert_in_delta(0.25, thread.total_time, 0.015)
+
+    root_methods = thread.root_methods.sort
+    assert_equal(2, root_methods.count)
+    assert_equal("BasicTest#start", root_methods[0].full_name)
+    assert_equal("BasicTest#test_leave_method", root_methods[1].full_name)
+
+    assert_equal(4, thread.methods.length)
+    methods = profile.threads.first.methods.sort
+
+    # Check times
+    assert_equal("<Class::RubyProf::C1>#sleep_wait", methods[0].full_name)
+    assert_in_delta(0.1, methods[0].total_time, 0.015)
+    assert_in_delta(0.0,  methods[0].wait_time, 0.015)
+    assert_in_delta(0.0,  methods[0].self_time, 0.015)
+
+    assert_equal("BasicTest#start", methods[1].full_name)
+    assert_in_delta(0.1, methods[1].total_time, 0.015)
+    assert_in_delta(0.0, methods[1].wait_time, 0.015)
+    assert_in_delta(0.0, methods[1].self_time, 0.015)
+
+    assert_equal("BasicTest#test_leave_method", methods[2].full_name)
+    assert_in_delta(0.15, methods[2].total_time, 0.015)
+    assert_in_delta(0.0, methods[2].wait_time, 0.015)
+    assert_in_delta(0.0, methods[2].self_time, 0.015)
+
+    assert_equal("Kernel#sleep", methods[3].full_name)
+    assert_in_delta(0.25, methods[3].total_time, 0.015)
+    assert_in_delta(0.0, methods[3].wait_time, 0.015)
+    assert_in_delta(0.25, methods[3].self_time, 0.015)
+  end
+
+  def test_leave_method_2
+    start
+    RubyProf::C1.sleep_wait
+    RubyProf::C1.sleep_wait
+    profile = RubyProf.stop
+
+    assert_equal(1, profile.threads.count)
+
+    thread = profile.threads.first
+    assert_in_delta(0.3, thread.total_time, 0.015)
+
+    root_methods = thread.root_methods.sort
+    assert_equal(2, root_methods.count)
+    assert_equal("BasicTest#start", root_methods[0].full_name)
+    assert_equal("BasicTest#test_leave_method_2", root_methods[1].full_name)
+
+    assert_equal(4, thread.methods.length)
+    methods = profile.threads.first.methods.sort
+
+    # Check times
+    assert_equal("BasicTest#start", methods[0].full_name)
+    assert_in_delta(0.1, methods[0].total_time, 0.015)
+    assert_in_delta(0.0, methods[0].wait_time, 0.015)
+    assert_in_delta(0.0, methods[0].self_time, 0.015)
+
+    assert_equal("BasicTest#test_leave_method_2", methods[1].full_name)
+    assert_in_delta(0.2, methods[1].total_time, 0.015)
+    assert_in_delta(0.0, methods[1].wait_time, 0.015)
+    assert_in_delta(0.0, methods[1].self_time, 0.015)
+
+    assert_equal("Kernel#sleep", methods[2].full_name)
+    assert_in_delta(0.3, methods[2].total_time, 0.015)
+    assert_in_delta(0.0, methods[2].wait_time, 0.015)
+    assert_in_delta(0.3, methods[2].self_time, 0.015)
+
+    assert_equal("<Class::RubyProf::C1>#sleep_wait", methods[3].full_name)
+    assert_in_delta(0.3, methods[3].total_time, 0.015)
+    assert_in_delta(0.0, methods[3].wait_time, 0.015)
+    assert_in_delta(0.0, methods[3].self_time, 0.015)
+  end
 end

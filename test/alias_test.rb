758f186657e76d9fa6ed2c47da6f759a958b68fa
Charlie Savage <cfis@zerista.com>
2019-07-29 17:34:23 -0700

    Fix line numbers again due to added comment.

--- test/alias_test.rb
+++ test/alias_test.rb
@@ -34,22 +34,22 @@ class AliasTest < TestCase
       # Method 0
       method = methods[0]
       assert_equal('AliasTest#test_alias', method.full_name)
-      assert_equal(26, method.line)
+      assert_equal(28, method.line)
       refute(method.recursive?)
 
       assert_equal(1, method.callers.count)
       call_info = method.callers[0]
       assert_nil(call_info.parent)
-      assert_equal(26, call_info.line)
+      assert_equal(28, call_info.line)
 
       assert_equal(2, method.callees.count)
       call_info = method.callees[0]
       assert_equal('Class#new', call_info.target.full_name)
-      assert_equal(26, call_info.line)
+      assert_equal(28, call_info.line)
 
       call_info = method.callees[1]
       assert_equal('AliasTest::TestMe#some_method', call_info.target.full_name)
-      assert_equal(26, call_info.line)
+      assert_equal(28, call_info.line)
 
       # Method 1
       method = methods[1]
@@ -60,7 +60,7 @@ class AliasTest < TestCase
       assert_equal(1, method.callers.count)
       call_info = method.callers[0]
       assert_equal('AliasTest#test_alias', call_info.parent.full_name)
-      assert_equal(26, call_info.line)
+      assert_equal(28, call_info.line)
 
       assert_equal(1, method.callees.count)
       call_info = method.callees[0]
@@ -89,7 +89,7 @@ class AliasTest < TestCase
       assert_equal(1, method.callers.count)
       call_info = method.callers[0]
       assert_equal('AliasTest#test_alias', call_info.parent.full_name)
-      assert_equal(26, call_info.line)
+      assert_equal(28, call_info.line)
 
       assert_equal(1, method.callees.count)
       call_info = method.callees[0]

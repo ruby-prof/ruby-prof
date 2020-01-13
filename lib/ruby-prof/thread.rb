832578707ebdad5af228f75de9cef2a913af92ae
Charlie Savage <cfis@savagexi.com>
2020-01-13 01:42:30 -0800

    Go back to call tree - using methods when climbing out of a call stack turn out also to be wrong so go with the faster wrong answer!

--- lib/ruby-prof/thread.rb
+++ lib/ruby-prof/thread.rb
@@ -2,11 +2,7 @@ module RubyProf
   class Thread
     # Returns the total time this thread was executed.
     def total_time
-      # TODO - this is what we should really do but have to fix the climbing out of call tree issue first
-      # self.call_tree.total_time
-      self.methods.reduce(0) do |total, method|
-        total += method.measurement.total_time
-      end
+      self.call_tree.total_time
     end
 
     # Returns the amount of time this thread waited while other thread executed.

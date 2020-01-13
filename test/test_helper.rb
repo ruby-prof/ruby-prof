09d97a6216d813261fcd410f716b5587abef7051
Charlie Savage <cfis@savagexi.com>
2020-01-12 21:29:35 -0800

    Use a more modern approach to debug in-tree (meaning you don't have the ruby-prof gem installed).

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,24 +1,12 @@
 # encoding: UTF-8
 
-require "rubygems"
-gem "minitest"
-require 'singleton'
-
-# To make testing/debugging easier, test within this source tree versus an installed gem
-dir = File.dirname(__FILE__)
-root = File.expand_path(File.join(dir, '..'))
-lib = File.expand_path(File.join(root, 'lib'))
-ext = File.expand_path(File.join(root, 'ext', 'ruby_prof'))
-
-$LOAD_PATH << lib
-$LOAD_PATH << ext
-
-require 'ruby-prof'
-
 # Disable minitest parallel tests. The problem is the thread switching will cahnge test results
 # (self vs wait time)
 ENV["N"] = "0"
+
+require 'bundler/setup'
 require 'minitest/autorun'
+require 'ruby-prof'
 
 class TestCase < Minitest::Test
 end

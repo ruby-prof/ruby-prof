b382bb86f1920b17cfe1ca4e1b8715430f911bce
Charlie Savage <cfis@savagexi.com>
2020-04-19 22:39:17 -0700

    Revert "Making it possible to aggregate several requests in the Rack middleware" That commit makes the rack adapter unsafe for threading, causing #262.
    This reverts commit 3ef8889d5c3eb3d14be1c4b666ca9234f1b04d38.

--- test/rack_test.rb
+++ test/rack_test.rb
@@ -24,16 +24,6 @@ module Rack
   end
 end
 
-module Rack
-  class RubyProf
-    attr_reader :_profiler
-
-    def public_delete_profiler!
-      delete_profiler!
-    end
-  end
-end
-
 class RackTest < TestCase
   def test_create_print_path
     path = Dir.mktmpdir
@@ -99,59 +89,5 @@ class RackTest < TestCase
 
     file_path = ::File.join(path, 'path-to-resource.json-dynamic.txt')
     assert(File.exist?(file_path))
-  end
-
-  def test_works_for_multiple_requests
-    path = Dir.mktmpdir
-
-    adapter = Rack::RubyProf.new(FakeRackApp.new, :path => path, :max_requests => 2)
-
-    # make a 1st request, and check that this didn't create any files
-    adapter.call(:fake_env)
-    assert(Dir["#{path}/*"].empty?)
-
-    # now a second request should create all the expected files
-    adapter.call(:fake_env)
-    %w(flat.txt graph.txt graph.html call_stack.html).each do |base_name|
-      file_path = ::File.join(path, "multi-requests-2-#{base_name}")
-      assert(File.exist?(file_path))
-    end
-
-    # let's clean up
-    FileUtils.rm_rf(Dir["#{path}/*"])
-
-    # and do the same again for the next 2 requests
-    adapter.call(:fake_env)
-    assert(Dir["#{path}/*"].empty?)
-
-    adapter.call(:fake_env)
-    %w(flat.txt graph.txt graph.html call_stack.html).each do |base_name|
-      file_path = ::File.join(path, "multi-requests-2-#{base_name}")
-      assert(File.exist?(file_path))
-    end
-  end
-
-  def test_tries_to_print_results_if_shut_down_before_max_requests_reached
-    path = Dir.mktmpdir
-
-    adapter = Rack::RubyProf.new(FakeRackApp.new, :path => path, :max_requests => 100)
-
-    # make a 1st request, and check that this didn't create any files
-    adapter.call(:fake_env)
-    assert(Dir["#{path}/*"].empty?)
-
-    adapter.public_delete_profiler!
-
-    %w(flat.txt graph.txt graph.html call_stack.html).each do |base_name|
-      file_path = ::File.join(path, "multi-requests-1-#{base_name}")
-      assert(File.exist?(file_path))
-    end
-  end
-
-  def test_it_uses_separate_profilers_if_not_aggregating_multiple_requests
-    adapter1 = Rack::RubyProf.new(FakeRackApp.new)
-    adapter2 = Rack::RubyProf.new(FakeRackApp.new)
-
-    assert(adapter1.object_id != adapter2.object_id)
   end
 end

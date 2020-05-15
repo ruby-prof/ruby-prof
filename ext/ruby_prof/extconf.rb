926e4659009e2a7ffc1d4fff522f3697f68ac6a6
Charlie Savage <cfis@savagexi.com>
2020-05-14 22:17:22 -0700

    More work on #272 - remove unneeded gcc warning since we are using c99.

--- ext/ruby_prof/extconf.rb
+++ ext/ruby_prof/extconf.rb
@@ -6,4 +6,7 @@ have_func('rb_tracearg_callee_id', ["ruby.h"])
 # We want to intermix declarations and code (ie, don't define all variables at the top of the method)
 $CFLAGS += ' -std=c99'
 
+# And since we are using C99 we want to disable Ruby sending these warnings to gcc
+CONFIG['warnflags'].gsub!('-Wdeclaration-after-statement', '')
+
 create_makefile("ruby_prof")

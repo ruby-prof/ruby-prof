21cd8d25ab97b1c710bd8ff8d97fb190fddfee74
Charlie Savage <cfis@savagexi.com>
2020-05-14 22:00:00 -0700

    Fix building on older versions of gcc/ubuntu. See #272.

--- ext/ruby_prof/extconf.rb
+++ ext/ruby_prof/extconf.rb
@@ -3,4 +3,7 @@ require "mkmf"
 # This function was added in Ruby 2.5, so once Ruby 2.4 is no longer supported this can be removed
 have_func('rb_tracearg_callee_id', ["ruby.h"])
 
+# We want to intermix declarations and code (ie, don't define all variables at the top of the method)
+$CFLAGS += ' -std=c99'
+
 create_makefile("ruby_prof")

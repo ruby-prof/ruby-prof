93333415dedc6ac6334a06c6f67a91164845999a
Charlie Savage <cfis@savagexi.com>
2020-08-23 22:40:44 -0700

    Bit of test cleanup.

--- test/printer_flat_test.rb
+++ test/printer_flat_test.rb
@@ -3,91 +3,96 @@
 
 require File.expand_path('../test_helper', __FILE__)
 require 'fileutils'
+require 'stringio'
 require 'tmpdir'
 require_relative 'prime'
 
 # --  Tests ----
 class PrinterFlatTest < TestCase
-  def setup
-    # WALL_TIME so we can use sleep in our test and get same measurements on linux and windows
-    RubyProf::measure_mode = RubyProf::WALL_TIME
-    @result = RubyProf.profile do
+  def run_profile
+    RubyProf.profile(:measure_mode => RubyProf::WALL_TIME) do
       run_primes(1000, 5000)
     end
   end
 
   def flat_output_nth_column_values(output, n)
-    only_method_calls = output.split("\n").select { |line| line =~ /^ +\d+/ }
-    only_method_calls.collect { |line| line.split(/ +/)[n] }
+    only_method_calls = output.split("\n").select { |line| line =~ /^\s+\d+/ }
+    only_method_calls.collect { |line| line.split(/\s+/)[n] }
   end
 
-  def assert_sorted array
-    array = array.map{|n| n.to_f} # allow for > 10s times to sort right, since lexographically 4.0 > 10.0
-    assert_equal array, array.sort.reverse, "Array #{array.inspect} is not sorted"
+  def helper_test_flat_string(klass)
+    output = StringIO.new
+
+    printer = klass.new(self.run_profile)
+    printer.print(output)
+
+    assert_match(/Thread ID: -?\d+/i, output.string)
+    assert_match(/Fiber ID: -?\d+/i, output.string)
+    assert_match(/Total: \d+\.\d+/i, output.string)
+    assert_match(/Object#run_primes/i, output.string)
+    output.string
   end
 
+  def assert_sorted(array)
+    array = array.map(&:to_f) # allow for > 10s times to sort right, since lexographically 4.0 > 10.0
+    assert_equal(array, array.sort.reverse)
+  end
+  
   def test_flat_string
     output = helper_test_flat_string(RubyProf::FlatPrinter)
     assert_match(/prime.rb/, output)
   end
 
-  def helper_test_flat_string(klass)
-    output = ''
-
-    printer = klass.new(@result)
-    printer.print(output)
-
-    assert_match(/Thread ID: -?\d+/i, output)
-    assert_match(/Fiber ID: -?\d+/i, output)
-    assert_match(/Total: \d+\.\d+/i, output)
-    assert_match(/Object#run_primes/i, output)
-    output
-  end
-
   def test_flat_result_sorting_by_self_time_is_default
-    printer = RubyProf::FlatPrinter.new(@result)
+    printer = RubyProf::FlatPrinter.new(self.run_profile)
 
-    printer.print(output = '')
-    self_times = flat_output_nth_column_values(output, 3)
+    output = StringIO.new
+    printer.print(output)
+    self_times = flat_output_nth_column_values(output.string, 3)
 
     assert_sorted self_times
   end
 
   def test_flat_result_sorting
-    printer = RubyProf::FlatPrinter.new(@result)
+    printer = RubyProf::FlatPrinter.new(self.run_profile)
 
     sort_method_with_column_number = {:total_time => 2, :self_time => 3, :wait_time => 4, :children_time => 5}
 
     sort_method_with_column_number.each_pair do |sort_method, n|
-      printer.print(output = '', :sort_method => sort_method)
-      times = flat_output_nth_column_values(output, n)
-      assert_sorted times
+      output = StringIO.new
+      printer.print(output, :sort_method => sort_method)
+
+      times = flat_output_nth_column_values(output.string, n)
+      assert_sorted(times)
     end
   end
 
   def test_flat_result_max_percent
-    printer = RubyProf::FlatPrinter.new(@result)
+    printer = RubyProf::FlatPrinter.new(self.run_profile)
 
-    printer.print(output = '', max_percent: 1)
-    self_percents = flat_output_nth_column_values(output, 1).map(&:to_f)
+    output = StringIO.new
+    printer.print(output, max_percent: 1)
+    self_percents = flat_output_nth_column_values(output.string, 1).map(&:to_f)
 
     assert self_percents.max < 1
   end
 
   def test_flat_result_filter_by_total_time
-    printer = RubyProf::FlatPrinter.new(@result)
+    printer = RubyProf::FlatPrinter.new(self.run_profile)
 
-    printer.print(output = '', filter_by: :total_time, min_percent: 50)
-    total_times = flat_output_nth_column_values(output, 2).map(&:to_f)
+    output = StringIO.new
+    printer.print(output, filter_by: :total_time, min_percent: 50)
+    total_times = flat_output_nth_column_values(output.string, 2).map(&:to_f)
 
     assert (total_times.min / total_times.max) >= 0.5
   end
 
   def test_flat_result_filter_by_self_time
-    printer = RubyProf::FlatPrinter.new(@result)
+    printer = RubyProf::FlatPrinter.new(self.run_profile)
 
-    printer.print(output = '', filter_by: :self_time, min_percent: 0.1)
-    self_percents = flat_output_nth_column_values(output, 1).map(&:to_f)
+    output = StringIO.new
+    printer.print(output, filter_by: :self_time, min_percent: 0.1)
+    self_percents = flat_output_nth_column_values(output.string, 1).map(&:to_f)
 
     assert self_percents.min >= 0.1
   end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,9 @@
 # encoding: UTF-8
 
-# Disable minitest parallel tests. The problem is the thread switching will cahnge test results
+# Disable minitest parallel tests. The problem is the thread switching will change test results
 # (self vs wait time)
-ENV["N"] = "0"
+ENV["N"] = "0" # Older versions of minitest
+ENV["MT_CPU"] = "0" # Newer versions minitest
 
 require 'bundler/setup'
 require 'minitest/autorun'

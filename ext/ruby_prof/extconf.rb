12bb62939c3bcde294f7bc43c3891c5ddab996d9
Charlie Savage <cfis@savagexi.com>
2021-02-06 00:56:16 -0800

    Remove Ruby 2.4 support since it is no longer maintained.

--- ext/ruby_prof/extconf.rb
+++ ext/ruby_prof/extconf.rb
@@ -1,8 +1,5 @@
 require "mkmf"
 
-# This function was added in Ruby 2.5, so once Ruby 2.4 is no longer supported this can be removed
-have_func('rb_tracearg_callee_id', ["ruby.h"])
-
 # Let's go with a modern version of C! want to intermix declarations and code (ie, don't define
 # all variables at the top of the method). If using Visual Studio, you'll need 2019 version
 # 16.8 or higher

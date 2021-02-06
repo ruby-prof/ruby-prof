659faf3bc2b3f2fe715fb836a17571ff04910924
Charlie Savage <cfis@savagexi.com>
2021-02-06 00:13:40 -0800

    Use C11.

--- ext/ruby_prof/extconf.rb
+++ ext/ruby_prof/extconf.rb
@@ -3,9 +3,13 @@ require "mkmf"
 # This function was added in Ruby 2.5, so once Ruby 2.4 is no longer supported this can be removed
 have_func('rb_tracearg_callee_id', ["ruby.h"])
 
-# We want to intermix declarations and code (ie, don't define all variables at the top of the method)
-unless RUBY_PLATFORM =~ /mswin/
-  $CFLAGS += ' -std=c99'
+# Let's go with a modern version of C! want to intermix declarations and code (ie, don't define
+# all variables at the top of the method). If using Visual Studio, you'll need 2019 version
+# 16.8 or higher
+if RUBY_PLATFORM =~ /mswin/
+  $CFLAGS += ' /std:c11'
+else
+  $CFLAGS += ' -std=c11'
 end
 
 # And since we are using C99 we want to disable Ruby sending these warnings to gcc

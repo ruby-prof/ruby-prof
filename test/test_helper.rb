025422a459aada6326263c0ee0ec9521ad44cd76
Charlie Savage <cfis@savagexi.com>
2022-12-17 15:07:43 -0800

    Really turn off minitest parallel runner.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,17 +1,13 @@
 # encoding: UTF-8
 
 require 'bundler/setup'
-require 'minitest/autorun'
+require 'ruby-prof'
 
 # Disable minitest parallel tests. The problem is the thread switching will change test results
 # (self vs wait time)
-if Gem::Version.new(Minitest::VERSION) > Gem::Version.new('5.11.3')
-  ENV["MT_CPU"] = "0" # Newer versions minitest
-else
-  ENV["N"] = "0" # Older versions of minitest
-end
+ENV["MT_CPU"] = "0" # New versions of minitest
+ENV["N"] = "0" # Older versions of minitest
 
-require 'ruby-prof'
-
+require 'minitest/autorun'
 class TestCase < Minitest::Test
 end

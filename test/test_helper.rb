0f1697ca1b52a6cd56bb45065926b134de469635
Charlie Savage <cfis@savagexi.com>
2023-02-21 23:30:41 -0800

    Try a different tact for loading extension when running tests locally.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,13 @@
 # encoding: UTF-8
 
+# To make testing/debugging easier test within this source tree versus an installed gem
 require 'bundler/setup'
+
+# Add ext directory to load path to make it easier to test locally built extensions
+ext_path = File.expand_path(File.join(__dir__, '..', 'ext', 'ruby_prof'))
+$LOAD_PATH.unshift(File.expand_path(ext_path))
+
+# Now load code
 require 'ruby-prof'
 
 # Disable minitest parallel tests. The problem is the thread switching will change test results

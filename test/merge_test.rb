989f4b75953dbfdec204dd23161f6939b081913c
Charlie Savage <cfis@savagexi.com>
2023-04-16 14:50:39 -0700

    Restore tests from master branch that were removed.

--- test/merge_test.rb
+++ test/merge_test.rb
@@ -8,7 +8,7 @@ require 'set'
 require_relative './scheduler'
 
 if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('3.0.0')
-puts "Hello"
+
 # --  Tests ----
 class MergeTest < TestCase
   def worker1
@@ -34,6 +34,18 @@ class MergeTest < TestCase
     scheduler = Scheduler.new
     Fiber.set_scheduler(scheduler)
 
+    3.times do
+      Fiber.schedule do
+        worker1
+      end
+    end
+    Fiber.scheduler.close
+  end
+
+  def concurrency_unmergable
+    scheduler = Scheduler.new
+    Fiber.set_scheduler(scheduler)
+
     3.times do |i|
       Fiber.schedule do
         method = "worker#{i + 1}".to_sym
@@ -43,19 +55,52 @@ class MergeTest < TestCase
     Fiber.scheduler.close
   end
 
-  def test_times_merge
+  def test_times_no_merge
     result  = RubyProf::Profile.profile(measure_mode: RubyProf::WALL_TIME) { concurrency_mergable }
-    result.merge!
-    assert_equal(2, result.threads.size)
+
+    assert_equal(4, result.threads.size)
+
     result.threads.each do |thread|
-      assert_in_delta(0.5, thread.call_tree.target.total_time, 0.5)
+      assert_in_delta(0.5, thread.call_tree.target.total_time, 0.2)
       assert_in_delta(0.0, thread.call_tree.target.self_time)
       assert_in_delta(0.0, thread.call_tree.target.wait_time)
-      assert_in_delta(0.5, thread.call_tree.target.children_time, 0.5)
+      assert_in_delta(0.5, thread.call_tree.target.children_time, 0.2)
     end
-    assert_equal(3,result.threads[1].call_tree.target.call_trees.callees.size)
   end
 
+  def test_times_mergable
+    result  = RubyProf::Profile.profile(measure_mode: RubyProf::WALL_TIME) { concurrency_mergable }
+    result.merge!
 
+    printer = RubyProf::GraphHtmlPrinter.new(result)
+    File.open("graph.html", "wb") do |file|
+      printer.print(file)
+    end
+
+    assert_equal(2, result.threads.size)
+
+    thread = result.threads[0]
+    assert_in_delta(0.5, thread.call_tree.target.total_time, 0.2)
+    assert_in_delta(0.0, thread.call_tree.target.self_time)
+    assert_in_delta(0.0, thread.call_tree.target.wait_time)
+    assert_in_delta(0.5, thread.call_tree.target.children_time, 0.2)
+
+    thread = result.threads[1]
+    assert_in_delta(1.5, thread.call_tree.target.total_time, 0.2)
+    assert_in_delta(0.0, thread.call_tree.target.self_time)
+    assert_in_delta(0.0, thread.call_tree.target.wait_time)
+    assert_in_delta(1.5, thread.call_tree.target.children_time, 0.2)
+  end
+
+  def test_unmergable
+    result  = RubyProf::Profile.profile(measure_mode: RubyProf::WALL_TIME) { concurrency_unmergable }
+
+    result.merge!
+
+    printer = RubyProf::GraphPrinter.new(result)
+    File.open("graph_merged.txt", "wb") do |file|
+      printer.print(file, :min_percent => 0)
+    end
+  end
 end
 end

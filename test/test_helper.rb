7740c0685235f31e7600e0f1b20a6d6ab1e57cb5
cfis <cfis@savagexi.com>
2025-05-21 20:09:43 -0700

    Don't need a second call to File.expand_ath

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,7 +5,7 @@ require 'bundler/setup'
 
 # Add ext directory to load path to make it easier to test locally built extensions
 ext_path = File.expand_path(File.join(__dir__, '..', 'ext', 'ruby_prof'))
-$LOAD_PATH.unshift(File.expand_path(ext_path))
+$LOAD_PATH.unshift(ext_path)
 
 # Now load code
 require 'ruby-prof'
